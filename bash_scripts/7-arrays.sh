40c9e5629a192826f6121031c54812a0a85d809c
Nuno Nogueira <nuno2nogueira@gmail.com>
2022-12-05 15:32:12 +0000

    Create 7-arrays.sh

---
+++ bash_scripts/7-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+array=(Paris Berlin London Lisbon)
+
+echo "Array position 1 is ${array[0]}"
+echo "Array position 2 is ${array[1]}"
+echo "Array position 3 is ${array[2]}"
+echo "Array position 4 is ${array[3]}"
