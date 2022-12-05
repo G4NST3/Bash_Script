3ea02431225a3137e6be5d2f0354540e3beeca59
Nuno Nogueira <102742284+G4NST3@users.noreply.github.com>
2022-12-05 21:25:42 +0000

    Update comments.sh

--- bash_scripts/comments.sh
+++ bash_scripts/comments.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 # Define a variable named xpto
 xpto="MaX"
-
 # Print the following text
 echo 'The variable is xpto'
-
 # Print the following text with xpto variable value
 echo "The variable value is $xpto"
