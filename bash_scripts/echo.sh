e3a1cd839773d9c3f493467b541e8accade1f877
Nuno Nogueira <nuno2nogueira@gmail.com>
2022-12-05 17:33:15 +0000

    Update echo.sh

--- bash_scripts/echo.sh
+++ bash_scripts/echo.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 message="Bash script is the best!"
-
 # print exactly the text as it is defined
 echo 'The message is: $message'
-
 # print the actual value of a variable
 echo "The message is: $message"
