cb86a1461b0f700b41e66a924572d7a767456ca8
Nuno Nogueira <nuno2nogueira@gmail.com>
2022-12-14 23:08:04 +0000

    Update if-then-else.sh

--- bash_scripts/if-then-else.sh
+++ bash_scripts/if-then-else.sh
@@ -5,8 +5,7 @@ answer=$[3+1]
 echo 'How much is 3+1?'
 read result
 
-if [ $result -eq $answer ]
-then
+if [ $result -eq $answer ]; then
     echo 'Congratulations! :) Wright answer!'
 else
     echo "Wrong answer. :( The result is $answer."
