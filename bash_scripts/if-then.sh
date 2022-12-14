cdda30248229a82844a6386f7b8f48922c761755
Nuno Nogueira <nuno2nogueira@gmail.com>
2022-12-14 22:55:26 +0000

    Create if-then.sh

---
+++ bash_scripts/if-then.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+answer=$[3+1]
+
+echo 'How much is 3+1?'
+read result
+
+if [ $result -eq $answer ]
+then
+    echo 'Congratulations! :) Wright answer!'
+else
+    echo "Wrong answer. :( The result is $answer."
+fi
