0d684e5ec4792cd5984f0b60868ef7b706493d8c
Nuno Nogueira <nuno2nogueira@gmail.com>
2022-12-16 15:40:16 +0000

    update

--- bash_scripts/ifnested.sh
+++ bash_scripts/ifnested.sh
@@ -1,30 +1,69 @@
 #!/bin/bash
 
-answer=$[3+1]
-
+result=$[3+1]
 echo 'How much is 3+1?'
-read result
+read answer
 
-if [ $result -eq $answer ]; then
-    echo 'Congratulations! :) Wright answer!'
-elif [ $result -gt $answer ]; then 
-    echo "Wrong answer. :( The result ($answer) is lower than your answer ($result)."
-elif [ $result -le $answer ]; then 
-    echo "Wrong answer. :( The result ($answer) is bigger than your answer ($result)."
+# Check if answer is not empty #################################################
+if [ -z $answer ]; then
+    echo "No answer given."
+    exit 1
 fi
 
-# This is another alternative #################################################
+# Check if values inserted are integer numbers #################################
+if ! [[ $answer =~ ^[0-9]+$ ]]; then
+    echo "Only integer numbers are valid."
+    exit 1
+fi
 
-: <<'COMMENT'
+# EXAMPLE 1 ####################################################################
+: <<'EXAMPLE_1'
 if [ $result -eq $answer ]; then
     echo 'Congratulations! :) Wright answer!'
 else
-    if [ $result -gt $answer ]; then 
-        echo "Wrong answer. :( The result ($answer) is lower than your answer ($result)."
+    if [ $result -le $answer ]; then 
+        echo "Wrong answer. :( The result ($result) is lower than your answer ($answer)."
     fi
 
-    if [ $result -le $answer ]; then 
-        echo "Wrong answer. :( The result ($answer) is bigger than your answer ($result)."
+    if [ $result -gt $answer ]; then 
+        echo "Wrong answer. :( The result ($result) is bigger than your answer ($answer)."
     fi
 fi
-COMMENT
+EXAMPLE_1
+
+# EXAMPLE 2 ####################################################################
+: <<'EXAMPLE_2'
+if [ $result -eq $answer ]; then
+    echo 'Congratulations! :) Wright answer!'
+else
+    if [ $result -le $answer ]; then 
+        echo "Wrong answer. :( The result ($result) is lower than your answer ($answer)."
+        elif [ $result -gt $answer ]; then 
+            echo "Wrong answer. :( The result ($result) is bigger than your answer ($answer)."
+    fi
+fi
+EXAMPLE_2
+
+# EXAMPLE 3 ####################################################################
+: <<'EXAMPLE_3'
+if [ $result -eq $answer ]; then
+    echo 'Congratulations! :) Wright answer!'
+else
+    if [ $result -le $answer ]; then 
+        echo "Wrong answer. :( The result ($result) is lower than your answer ($answer)."
+        else
+        echo "Wrong answer. :( The result ($result) is bigger than your answer ($answer)."
+    fi
+fi
+EXAMPLE_3
+
+# EXAMPLE 4 ####################################################################
+#: <<'EXAMPLE_4'
+if [ $result -eq $answer ]; then
+    echo 'Congratulations! :) Wright answer!'
+elif [ $result -le $answer ]; then 
+        echo "Wrong answer. :( The result ($result) is lower than your answer ($answer)."
+        else
+        echo "Wrong answer. :( The result ($result) is bigger than your answer ($answer)."
+fi
+#EXAMPLE_4
